d730e2ccb0f664660c97b288fc498b20c58955f7
jilliandike <43615050+jilliandike@users.noreply.github.com>
2025-02-06 15:54:00 -0700

    Adds the modal information - WIP

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -1,22 +1,29 @@
 let people = [];
 const gallery = document.querySelector('.gallery');
+const modalContent = document.querySelector('.modal');
+const modalContainer = document.querySelector('.modal-container');
+const closeBtn = document.querySelector('.modal-close-btn');
+
+
+//Credit for function formatting in this file to TeamTreehouse lessons:
+//Practice Fetch API and Practice DOM Manipulation: Modal
 
 //FETCH DATA
 
-//Function formatting credit to https://teamtreehouse.com/workspaces/42270699#
+//Fetches the picture and info for 12 random users
 async function getPeople() {
     const response = await fetch(
       'https://randomuser.me/api/?results=12&inc=picture,name,email,location,phone,dob'
     );
     people = await response.json();
-    // console.log(people);
+    console.log(people); //remove upon polish
     displayPeople(people.results);
     return people;
   }
 
 //DISPLAY DATA GALLERY
 
-// Uses the API response data to display 12 random users, along with some basic information for each
+// Uses the API response data to display random users along with some basic information for each
 function displayPeople(people) {
     const galleryHTML = people
     .map(
@@ -41,4 +48,38 @@ getPeople();
 
 //DISPLAY MODAL
 
+//Opens the modal when a person is clicked LOGS THE CLICKED PERSON
+gallery.addEventListener('click', (event) => {
+    const personCard = event.target.closest('.card');
 
+    if(!personCard) return;
+
+    const clickedPerson = personCard.querySelector("#name").textContent.split(' ');
+    console.log(clickedPerson); //remove upon polish
+    const person = people.results.find(
+        (person) => person.name.first && person.name.last === clickedPerson
+    );
+    displayPersonModal(person);
+});
+
+//Adds the person's information to the modal NOT WORKING - ERROR IN THE INTERPOLATION
+function displayPersonModal(person){
+    let personModalHTML = `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src=${person.picture.thumbnail} alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                    <p class="modal-text">${person.email}</p>
+                    <p class="modal-text cap">${person.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${person.phone}</p>
+                    <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${person.dob.date}</p>
+            </div>
+        </div>
+    `;
+    // modalContainer.innerHTML = personModalHTML; 
+    gallery.insertAdjacentHTML('beforeend', personModalHTML);
+}
